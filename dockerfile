162624a30eea1f77dc30f1556f5eb8afc9ecbc37
JulienNirxo <millaud.julien@gmail.com>
2024-01-17 16:27:04 +0100

    [ADD] bdd mysql

--- dockerfile
+++ dockerfile
@@ -7,13 +7,20 @@ RUN apk --update add \
     php8-cli \
     php8-mbstring \
     php8-json \
-    php8-session
+    php8-session \
+    php8-pdo \
+    php8-pdo_mysql
+
+# Installer MySQL
+RUN apk --update add \
+    mysql \
+    mysql-client
 
 # Copier les fichiers de votre application PHP
-COPY . /test-docker-alpine-php
+COPY . /alpinephp
 
 # Définir le répertoire de travail
-WORKDIR /test-docker-alpine-php
+WORKDIR /alpinephp
 
 # Exposer le port par défaut de PHP
 EXPOSE 80
--- index.php
+++ index.php
@@ -1,4 +1,32 @@
 <?php
-echo "Hello Jody !";
-echo '<img src="public/img/memedev2.jpg" alt="meme">';
+// Paramètres de connexion MySQL
+$host = 'mysql-container';
+$dbname = 'mydatabase';
+$user = 'root';
+$pass = 'password';
+
+// Connexion à MySQL
+try {
+    $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+} catch (PDOException $e) {
+    die('Connexion échouée : ' . $e->getMessage());
+}
+
+// Création de la table
+$query = 'CREATE TABLE IF NOT EXISTS mytable (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255))';
+$db->exec($query);
+
+// Insertion de données
+$query = 'INSERT INTO mytable (name) VALUES ("Jody")';
+$db->exec($query);
+
+// Récupération des données
+$result = $db->query('SELECT * FROM mytable');
+foreach ($result as $row) {
+    echo 'Salut ' . $row['name'] . '<br>';
+    echo '<img src="/public/img/memedev2.jpg">';
+}
+
+// Fermer la connexion à la base de données
+$db = null;
 ?>
